1b6bb8cbd0b799a5e54b93ace3d4cc1c1880ad64
vvargin <v.vargin@team.amocrm.com>
2016-04-02 23:51:53 +0300

    Unneeded todo

--- tests/result/actions.js
+++ tests/result/actions.js
@@ -121,7 +121,6 @@ describe('result', function () {
 
         it('Important check', function () {
           props.important.should.to.be.a('boolean');
-          // @TODO: Change that
           props.important.should.to.equal(_.indexOf(['lead_add', 'contact_add', 'contact_update'], key) !== -1);
         });
 
